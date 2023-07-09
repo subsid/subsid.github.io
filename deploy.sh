8e2ee6b75202f97add16bf146ddd2e56de14fe2d
Siddharth <ssubramaniyam@etsy.com>
2023-07-09 11:36:03 -0400

    rebuilding site Sun Jul  9 11:36:03 AM EDT 2023

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,15 @@ echo "Building site"
 # Build site
 hugo
 
+echo "Committing changes"
+# Commit changes
+git add -A
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
 echo "Deploying to main"
 # Deploy to main
 git push origin `git subtree split --prefix public org-hugo-site`:main --force
