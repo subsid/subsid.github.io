7874cacd6ca98d7ffb7d3a5fc6267db5db3b43be
Siddharth <ssubramaniyam@etsy.com>
2025-06-09 19:25:40 -0400

    Minor fixes

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = "watch" ]; then
     echo "Watching for changes in ~/Dropbox/notes/org_roam_v2/pages/article/..."
-    find ~/Dropbox/notes/org_roam_v2/pages/article/ \( -name "*.org" -o -name "*.css" \) -not -name "articles.org" -not -name "snippets.org" | entr -r ./build.sh
+    find ~/Dropbox/notes/org_roam_v2/pages/article/ \( -name "*.org" -o -name "*.css" \) -not -name "articles.org" -not -name "snippets.org" -not -name "all-articles.org"| entr -r ./build.sh
 else
     echo "Building site..."
     emacs -Q --script build-site.el
